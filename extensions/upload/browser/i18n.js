4e750f985c26ade009b0e80ad76e737d8916f31e
René Giovanni Borella <rgb@mapster.dk>
2026-01-08 10:15:57 +0100

    Feature/upload (#10)
    Ported upload, added license

---
+++ extensions/upload/browser/i18n.js
@@ -0,0 +1,76 @@
+/*
+ * @author     Gunnar Jul Jensen <gjj@geopartner.dk>
+ * @copyright  2025 Geopartner Landinspektører A/S
+ * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
+ */
+
+var dict = {
+    'Start loading': {
+      da_DK:
+        "Indlæser",
+      en_US:
+        "Start loading",
+    },
+    'Error: Must be a .zip file': {
+      da_DK:
+        "Fejl: Skal være en .zip fil",
+      en_US:
+        "Error: Must be a .zip file",
+    },
+    'Error: First file is not a zip file': {
+      da_DK:
+        "Fejl: Første fil er ikke en zip fil",
+      en_US:
+        "Error: First file is not a zip file",
+    },
+    'Error: Conversion to layer error': {
+      da_DK:
+        "Fejl: Konvertering til lag fejlede",
+      en_US:
+        "Error: Conversion to layer error",
+    },
+    "Error: No .shp file found in zip": {
+      da_DK:
+        "Fejl: Ingen .shp fil fundet i zip",
+      en_US:
+        "Error: No .shp file found in zip",
+    },
+    'Error unpacking file': {
+      da_DK:
+        "Fejl ved udpakning af fil",
+      en_US:
+        "Error unpacking file",
+    },
+    'Error on upload': {
+      da_DK:
+        "Fejl ved upload",
+      en_US:
+        "Error on upload",
+    },
+    "Error: No features found in file": {
+      da_DK:
+        "Fejl: Ingen objekter fundet i fil",
+      en_US:
+        "Error: No features found in file",
+    },
+    "File contains": {
+      da_DK:
+        "Filen indeholder",
+      en_US:
+        "File contains",
+    },
+    "objects": {
+      da_DK:
+        "objekter",
+      en_US:
+        "objects",
+    },
+    "File parsed successfully": {
+      da_DK:
+        "Filen blev indlæst korrekt",
+      en_US:
+        "File parsed successfully",
+    },
+  };
+
+  module.exports = dict;
