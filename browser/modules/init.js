76c6a4a1a704ee30f9f26a34c6f347ed835ff0e7
René Giovanni Borella <rgb@mapster.dk>
2026-04-17 12:33:17 +0200

    allowDownloadLayer / showLayerGroupChildren (#55)
    * allowDownloadLayer
    * showLayerGroupChildren

--- browser/modules/init.js
+++ browser/modules/init.js
@@ -106,6 +106,8 @@ module.exports = {
             loadCheckingInterval: 15000,
             mode: 0,
             layerTreeFilterPlaceholder: null,
+            allowDownloadLayer: true,
+            showLayerGroupChildren: true,
         };
         // Set default for unset props
         for (let prop in defaults) {
