f6de4320ebf6c543cd0746167927bf833a023da3
René Giovanni Borella <rgb@mapster.dk>
2026-09-22 14:30:06 +0200

    2026.9.0 (#79)
    * Prelim. simple merge of 2026.9.0
    * Merge ignorelist into streetview
    * merge allowDownload, hasFilter and other customs

--- browser/i18n/en_US.js
+++ browser/i18n/en_US.js
@@ -240,6 +240,7 @@ module.exports = {
         "Filter editor": "Filter editor",
         "Layer is visible": "Layer is visible",
         "Layer has filter": "Layer has filter",
+        "Layer has error": "Layer has error",
         "Parents": "Parents",
         "Children": "Children",
         "Back": "Back",
@@ -251,6 +252,8 @@ module.exports = {
         "Filter is required for showing data": "Filter is required for showing data",
         "More tools": "More tools",
         "Change layer type": "Change layer type",
+        "Creating download file": "Creating download file",
+        "File was downloaded": "File was downloaded",
 
         // Base layers
         "Side-by-side mode": "Side-by-side mode",
